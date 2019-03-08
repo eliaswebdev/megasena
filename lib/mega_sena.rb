27a4f63b93ede6f4bed67657fafa7ee6620d18ee
Elias Ferreira Junior <eliaswebdev@gmail.com>
2019-03-08 16:39:33 -0300

    test initializer

--- lib/mega_sena.rb
+++ lib/mega_sena.rb
@@ -1,34 +1,32 @@
 class MegaSena
   attr_accessor :numeros
 
-  def initialize(numeros=(1..60).to_a)
+  def initialize(numeros = (1..60).to_a)
     @numeros = numeros
   end
-  
+
   def jogo_normal(qtd_jogos)
-    
-    mistura_numeros(self.numeros, qtd_jogos)
+    mistura_numeros(numeros, qtd_jogos)
 
     total(qtd_jogos)
   end
-  
+
   def jogo_esperto(qtd_jogos)
-    mega = [04, 05, 07, 12, 13, 16, 17, 23, 24, 29, 30, 32, 33, 37, 38, 41, 42, 43, 47, 49, 50, 51, 53, 54, 58, 59]
-    
+    mega = [0o4, 0o5, 0o7, 12, 13, 16, 17, 23, 24, 29, 30, 32, 33, 37, 38, 41, 42, 43, 47, 49, 50, 51, 53, 54, 58, 59]
+
     mistura_numeros(mega, qtd_jogos)
 
     total(qtd_jogos)
   end
 
-  
   def jogo_mais_esperto(qtd_jogos)
-    range_normal = self.numeros.shuffle
-    range_seleto = [04, 05, 07, 12, 13, 16, 17, 23, 24, 29, 30, 32, 33, 37, 38, 41, 42, 43, 47, 49, 50, 51, 53, 54, 58, 59].shuffle
+    range_normal = numeros.shuffle
+    range_seleto = [0o4, 0o5, 0o7, 12, 13, 16, 17, 23, 24, 29, 30, 32, 33, 37, 38, 41, 42, 43, 47, 49, 50, 51, 53, 54, 58, 59].shuffle
     diff_range = range_normal - range_seleto
-    
+
     qtd_jogos.times do
-      mega = range_seleto.shuffle[0..3] + diff_range.shuffle[0..1]
-      p mega[0..5].sort.map {|n| n < 10 ? "0#{n}" : "#{n}" }.join(' - ')
+      mega = range_seleto.sample(4) + diff_range.sample(2)
+      p mega[0..5].sort.map { |n| n < 10 ? "0#{n}" : n.to_s }.join(' - ')
     end
     total(qtd_jogos)
   end
@@ -36,20 +34,18 @@ class MegaSena
   def mistura_numeros(mega, qtd_jogos)
     qtd_jogos.times do
       mega = mega.shuffle
-      p mega[0..5].sort.map {|n| n < 10 ? "0#{n}" : "#{n}" }.join(' - ')
+      p mega[0..5].sort.map { |n| n < 10 ? "0#{n}" : n.to_s }.join(' - ')
     end
-    
   end
-  
+
   def total(qtd_jogos)
     puts '========================================='
-    puts "TOTAL A PAGAR R$ #{qtd_jogos*3.5}"
+    puts "TOTAL A PAGAR R$ #{qtd_jogos * 3.5}"
     puts '========================================='
   end
-
 end
 
-meu_jogo = MegaSena.new
-meu_jogo.jogo_normal(4)
-meu_jogo.jogo_esperto(2)
-meu_jogo.jogo_mais_esperto(2)
+# meu_jogo = MegaSena.new
+# meu_jogo.jogo_normal(4)
+# meu_jogo.jogo_esperto(2)
+# meu_jogo.jogo_mais_esperto(2)
